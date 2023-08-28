0881b7fb6fc06541db96dccce536ae797e934aff
Selim_eh58 <Selim_eh58@STHQ0118-13>
2023-08-28 08:44:31 +0400

    Add project files.

---
+++ TicTacToe-Client/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int port = 27001;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToServer();
+            RequestLoop();
+        }
+
+        private void RequestLoop()
+        {
+            var receiver = Task.Run(() =>
+            {
+                while (true)
+                {
+                    ReceiveResponse();
+                }
+            });
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            IntegrateToView(text);
+        }
+
+        private void IntegrateToView(string text)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                var data = text.Split('\n');
+                var row1 = data[0].Split('\t');
+                var row2 = data[1].Split('\t');
+                var row3 = data[2].Split('\t');
+
+                Button1.Content = row1[0];
+                Button2.Content = row1[1];
+                Button3.Content = row1[2];
+
+                Button4.Content = row2[0];
+                Button5.Content = row2[1];
+                Button6.Content = row2[2];
+
+                Button7.Content = row3[0];
+                Button8.Content = row3[1];
+                Button9.Content = row3[2];
+            });
+        }
+
+        private void ConnectToServer()
+        {
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    ClientSocket.Connect(IPAddress.Parse("10.1.18.13"), port);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            MessageBox.Show("Connected!", "Successfully!", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+
+            string text = Encoding.ASCII.GetString(data);
+            this.Title = "Player: " + text;
+            this.player.Text = this.Title;
+
+            if (text.Contains("\t"))
+            {
+                MessageBox.Show("Game has started!", "Successfully!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    var bt = sender as Button;
+                    string request = bt.Content.ToString() + player.Text.Split(' ')[2];
+                    SendString(request);
+                });
+            });
+        }
+
+        private void SendString(string request)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(request);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+    }
+}
